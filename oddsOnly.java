f6796abe53399a28f0381cd7f416aaf0c3d273d5
ldorilas <43239931+ldorilas@users.noreply.github.com>
2019-01-20 14:25:01 -0500

    Create oddsOnly.java

---
+++ oddsOnly.java
@@ -0,0 +1,8 @@
+/**
+Consider the following incomplete method definition.
+
+public String oddsOnly(String original) {
+
+}
+original can be a valid String of unknown length, but might also be empty or null. Your method needs to account for each of these possibilities. The method should combine the odd-numbered characters of original and return the newly created String. If the String is null, return null.
+*/
