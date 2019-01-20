76ae07ffb1ac9b23a6bb31d4b5a6fef6b8d216b4
ldorilas <43239931+ldorilas@users.noreply.github.com>
2019-01-20 14:23:58 -0500

    Create Change.java

---
+++ Change.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+/**
+Consider the following incomplete method definition.
+
+public int coins(double change) {
+
+}
+change is guaranteed to be a non-negative value in the range [0.00, 1.00). Your method should calculate and return the fewest number of coins required to form this amount. No typecasting allowed.
+*/
