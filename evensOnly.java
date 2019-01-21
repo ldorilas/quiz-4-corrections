1eb34ea12751ad6ccff3b1f8c892a53d7c54ecdf
ldorilas <43239931+ldorilas@users.noreply.github.com>
2019-01-21 00:16:28 -0500

    Create evensOnly.java

---
+++ evensOnly.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+/**
+Consider the following incomplete method definition.
+
+public String evensOnly(String original) {
+
+}
+original can be a valid String of unknown length, but might also be empty or null. Your method needs to account for each of these possibilities. The method should combine the even-numbered characters of original and return the newly created String. If the String is null, return null.
+*/
