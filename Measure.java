b87a50917e06f9c40f2e43ac21267cd747b5047f
ldorilas <43239931+ldorilas@users.noreply.github.com>
2019-01-21 23:06:59 -0500

    measure

--- Measure.java
+++ Measure.java
@@ -1,8 +1,28 @@
 import java.util.Scanner;
-
 public class Measure {
-
-public String inches(double inches) [
   
+public static void main(String[] args) {
+  Scanner input = new Scanner(System.in);
+  
+  System.out.println("Enter number of inches.");
+  double inches = input.nextDouble();
+  
+  Measure length = new Measure();
+  length.mile = inches / 63360;
+  length.yard = inches / 36;
+  length.foot = inches / 12;
+  
+  input.close();
+  }
+  
+  private static double mile;
+  private static double yard;
+  private static double foot;
+  
+public String inches(double inches) {
+    mile = inches / 63360;
+    yard = inches / 36;
+    foot = inches / 12;
+    System.out.println(mile + " miles, " + yard + " yards, and " + foot + " feet.");
   }
 }
