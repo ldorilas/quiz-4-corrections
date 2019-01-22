91a8f4fcbad7484742f26c9b83beb78be5d9a4ff
ldorilas <43239931+ldorilas@users.noreply.github.com>
2019-01-21 23:09:26 -0500

    Update Change.java

--- Change.java
+++ Change.java
@@ -5,28 +5,27 @@ Consider the following incomplete method definition.
 change is guaranteed to be a non-negative value in the range [0.00, 1.00). Your method should calculate and return the fewest number of coins required to form this amount. No typecasting allowed.
 */
 
+public class Measure {
+  
+public static void main(String[] args) {
+  Scanner input = new Scanner(System.in);
+  
+  System.out.println("Enter number of inches.");
+  double  = input.nextDouble();
+  
+  Measure length = new Measure();
+  length.mile = inches / 63360;
+  length.yard = inches / 36;
+  length.foot = inches / 12;
+  
+  input.close();
+  }
+
 Scanner input = new Scanner(System.in);
 double change = input.nextDouble(); //user input for amount in cents; output should be least number of coins that makes up this amount
 // change = 0.04; coins = pennies * 4
 public class Change {
-  public int coins(double change) {
-    int coins = 0;
-    int penny = 1;
-    int nickel = 5;
-    int dime = 10;
-    int quarter = 25;
-    int amount;
-    if (change >= 0.01 && change < 0.05) {
-        penny = change * 100
-        coins = 1
-      }
-    else if (change >= 0.05 && change < 0.10) {
-        
-        coins = 2;
-        
-      }
-    }
-  }
+
 }
 
 
